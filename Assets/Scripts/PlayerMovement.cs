c16a07f6d86bc4d8c2c87ff4c2820f258319dfae
mohamedsaied38 <unity4677@gmail.com>
2022-07-16 15:50:09 +0200

    Chapter 5
    complete chapter 5 assignment

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rigidbody2d;
+    public float speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            rigidbody2d.velocity = new Vector2(speed, 0);
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            rigidbody2d.velocity = new Vector2(-speed, 0);
+        }
+
+        else if (Input.GetAxis("Vertical") > 0)
+        {
+            rigidbody2d.velocity = new Vector2(0, speed);
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            rigidbody2d.velocity = new Vector2(0, -speed);
+
+        }
+
+        else if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0 )//&& Input.GetKey(KeyCode.Space))
+        {
+            rigidbody2d.velocity = Vector2.zero;
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Door"))
+        {
+            Debug.Log("Level Complete");
+        }
+    }
+}
